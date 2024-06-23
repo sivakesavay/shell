d0c815af04c9ea646f1ecfaf06af1a97183f22fd
Siva kesava Yemineni <kesava8121@gmail.com>
2024-06-23 14:40:37 +0530

    justreadme

--- variables.sh
+++ variables.sh
@@ -4,5 +4,5 @@ plan1=masters
 plan2=students
 plan3=parents
 
-echo "today $plan2 were went to hiddencastle along with $plan1, and all the plan2 were got permission
+echo "today $plan2 were went to hiddencastle along with $plan1, and all the $plan2 were got permission
 from there $plan3 and all $plan3, $plan2, $plan1 are very happy today"
