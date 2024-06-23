24a9e0d55c16c4d03cf842e44dfb770923179c92
Siva kesava Yemineni <kesava8121@gmail.com>
2024-06-23 14:37:38 +0530

    justreadme

--- variables.sh
+++ variables.sh
@@ -5,4 +5,4 @@ plan2=students
 plan3=parents
 
 today $plan2 were went to hiddencastle along with $plan1, and all the plan2 were got permission
-from there $plan3
+from there $plan3 and all $plan3, $plan2, $plan1 are very happy today
