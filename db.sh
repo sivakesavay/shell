9296e3d7e3544e086ab04188ff478874031b560f
Siva kesava Yemineni <kesava8121@gmail.com>
2024-06-29 14:22:28 +0530

    db creating

--- db.sh
+++ db.sh
@@ -10,6 +10,8 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+echo "please enter db password:"
+read -s mysql_root_password
 validate(){
     if [$1 -ne 0]
     then
@@ -37,6 +39,15 @@ validate $? "enabling mysql server"
 systemctl start mysqld &>>logfile
 validate $? "starting my sql server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>logfile
-validate $? "settingup root password"
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>logfile
+# validate $? "settingup root password"
 
+# Idempotent nature
+ mysql -h db.jasdevops.cloud -uroot -p${mysql_root_password} -e 'show datbases;' &>>logfile
+ if [ $? -ne 0 ]
+ then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>logfile
+validate $? "mysql root passwod setup"
+else
+    echo -e "mysql root password is already setup...$Y SKIPPING $N"
+fi
